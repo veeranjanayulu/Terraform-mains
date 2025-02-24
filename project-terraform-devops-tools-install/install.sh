29f5e68d9404928c54d514ab30c6cd1a76cd8352
veeranjanayulu <141625717+veeranjanayulu@users.noreply.github.com>
2025-02-24 17:18:30 +0530

    Update install.sh

--- project-terraform-devops-tools-install/install.sh
+++ project-terraform-devops-tools-install/install.sh
@@ -50,6 +50,7 @@ sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v0.48.3/tr
 
 #----------------------sonarQube install-----------------------------------
 sudo yum -y install wget nfs-utils
+sudo dnf install java-11-amazon-corretto
 sudo wget -O /etc/yum.repos.d/sonar.repo http://downloads.sourceforge.net/project/sonar-pkg/rpm/sonar.repo
 sudo yum -y install sonar
 #-----------------------JFROg-----------------------------
